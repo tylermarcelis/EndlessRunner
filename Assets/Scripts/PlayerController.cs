f3f9f693b31a44554ee97507ed9574f1b338b7f2
Tyler Marcelis <s181428@students.aie.edu.au>
2019-12-13 09:41:49 +1030

    - Added continuous player movement, which should be working with gravity adjustments

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,12 +7,13 @@ public class PlayerController : MonoBehaviour
 {
     protected new Rigidbody2D rigidbody;
 
-    [SerializeField]
-    protected float minJumpHeight = 1;
+    public float minJumpHeight = 1;
     [SerializeField]
     protected float jumpHoldForce = 1;
+    [SerializeField]
+    protected float jumpHoldDuration = 1;
 
-    public float jumpHoldDuration = 1;
+    public float moveSpeed = 1;
 
     protected bool isGrounded;
 
@@ -23,24 +24,43 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        Move();
+
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
             StartCoroutine(Jump());
         }
+
+    }
+
+    private void FixedUpdate()
+    {
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        foreach (var contact in collision.contacts)
+        if (!isGrounded)
         {
-            if (Vector2.Dot(contact.normal, Physics2D.gravity.normalized) < -0.7f)
-                isGrounded = true;
+            foreach (var contact in collision.contacts)
+            {
+                if (Vector2.Dot(contact.normal, Physics2D.gravity.normalized) < -0.7f)
+                    isGrounded = true;
+            }
         }
     }
 
+    void Move()
+    {
+        float gravStrength = Vector2.Dot(Physics2D.gravity.normalized, rigidbody.velocity);
+        Vector2 gravVector = Physics2D.gravity.normalized * gravStrength;
+
+        Vector2 runDir = Vector2.Perpendicular(Physics2D.gravity.normalized);
+        rigidbody.velocity = runDir * moveSpeed + gravVector;
+    }
 
     IEnumerator Jump()
     {
+        // Calculate initial jump force, and get gravity direction
         Vector2 jumpDir = -Physics2D.gravity.normalized;
         float minForce = CalculateJumpForce(minJumpHeight);
         isGrounded = false;
@@ -51,6 +71,7 @@ public class PlayerController : MonoBehaviour
 
         float remainingTime = 1;
 
+        // While holding jump, continuously add force to increase jump height
         while (Input.GetButton("Jump") && remainingTime > 0)
         {
             rigidbody.AddForce(jumpDir * jumpHoldForce * remainingTime);
