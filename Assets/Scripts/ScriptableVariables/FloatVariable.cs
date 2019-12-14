7a298daa60fdb671702b3a5dfa13eecffbf822c0
Tyler Marcelis <s181428@students.aie.edu.au>
2019-12-14 23:10:47 +1030

    - Added coin pickup which increases score

--- Assets/Scripts/ScriptableVariables/FloatVariable.cs
+++ Assets/Scripts/ScriptableVariables/FloatVariable.cs
@@ -1,7 +1,19 @@
 ﻿using UnityEngine;
 
 [CreateAssetMenu(fileName = "float_", menuName = "Variables/FloatVariable")]
-public class FloatVariable : GenericVariable<float> { }
+public class FloatVariable : GenericVariable<float>
+{
+    public void Add(float value)
+    {
+        Value += value;
+    }
+}
 
 [System.Serializable]
-public class FloatReference : GenericReference<float, FloatVariable> { }
+public class FloatReference : GenericReference<float, FloatVariable>
+{
+    public void Add(float value)
+    {
+        Value += value;
+    }
+}
